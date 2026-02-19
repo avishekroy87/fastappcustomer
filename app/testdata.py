866e4215ba8be87f6463f8ea27d5623bd9996123
Avishek roy <avishekroy87@gmail.com>
2026-02-19 22:54:47 +0530

    19Feb

--- app/testdata.py
+++ app/testdata.py
@@ -5,3 +5,11 @@ marks = ["90", "80"]
 res = [f"{n}: {m}" for n, m 
                 in zip(names, marks)]
 print(res)
+
+
+x = 5
+y = 0
+while x > 0:
+    y = y * 10 + x
+    x = x - 1
+print(" " + str(y))
